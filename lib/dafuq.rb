ca06f833726e1ae8505ee374306b1c13b0ccfd8d
AbhishekKr <abhikumar163@gmail.com>
2013-02-12 00:19:12 +0530

    dafuq code trailing  whitespace fix util

--- lib/dafuq.rb
+++ lib/dafuq.rb
@@ -1,5 +1,24 @@
-require "dafuq/version"
+dafuq_all = File.join(File.dirname(File.expand_path __FILE__), '*', '*.rb')
+Dir.glob(dafuq_all).each {|lib| puts lib ; require lib}
 
 module Dafuq
-  # Your code goes here...
+
+  class << self
+    attr_accessor :verbose, :log
+  end
+
+  def self.log_me(msg)
+    puts "[+] #{msg}" if @verbose
+    File.open(@log, 'a+'){|fyl| fyl.puts msg} if File.exists?(@log)
+  end
+
+  def self.is_this
+    Dafuq.verbose = Arg0::Console.switch?(['-v', '--verbose'])
+    Dafuq.log     = Arg0::Console.value_for(['-l', '--log']).join
+    recurse       = ! Arg0::Console.switch?(['-no-r', '--no-recurse'])
+
+    Arg0::Console.value_for(['-notail', '--no-whitespace']).each do |no_whitespace_path|
+      Dafuq::Code.notail no_whitespace_path, recurse
+    end
+  end
 end
---
+++ lib/dafuq/code.rb
@@ -0,0 +1,18 @@
+module Dafuq
+  module Code
+
+    # 'remove tailing whitespace'
+    def notail(edit_path, recursive = true)
+      Dir.glob(File.expand_path(File.join edit_path, '*')).each do |fyl|
+        if File.symlink? fyl
+          next
+        elsif File.directory?(fyl) && recursive
+          notail(fyl)
+        else
+          Dafuq.log_me "Processing: #{fyl}"
+          %x{sed -i 's/\ *$//g' #{fyl}}
+        end
+      end
+    end
+  end
+end
